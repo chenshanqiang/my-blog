6fa3f3cffb938f829deb9bd6e88c3ca4495cabbf
chenshanqiang <chenshanqi.ang@163.com>
2019-12-31 11:56:32 +0800

    ''

---
+++ controller/admin/userEdit.js
@@ -0,0 +1,4 @@
+/* jshint esversion: 8*/
+module.exports = (req, res) => {
+    res.render('admin/user-edit')
+}
--- routes/admin.js
+++ routes/admin.js
@@ -8,4 +8,10 @@ router.get('/login', require('../controller/admin/loginPage'));
 router.post('/login', require('../controller/admin/login'));
 // 实现退出登录功能
 router.get('/logout', require('../controller/admin/logout'));
+// 渲染添加用户界面
+router.get('/user-edit', require('../controller/admin/userEdit'));
+// 提交新用户数据
+router.post('/user-edit-fn', require('../controller/admin/userEditFn'));
+
+
 module.exports = router;
